ef7171918b038c7fb1b35d77248e59f626ea7a4b
arunthomasalex <arunthomasalex@gmail.com>
2021-05-09 18:27:12 +0530

    Minor change on the webpack configuration part.

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,54 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const RemovePlugin = require('remove-files-webpack-plugin');
 
 module.exports = {
-	entry: path.join(__dirname, "src", "index.js"),
+	entry: path.join(__dirname, 'src', 'index.js'),
 	output: { 
-		path: path.join(__dirname, "public"), 
-		filename: "build.[hash].js" 
+		path: path.join(__dirname, 'public'), 
+		filename: 'build.[hash].js' 
 	},
-	mode: process.env.NODE_ENV || "development",
+	mode: process.env.NODE_ENV || 'development',
+	// devtool: (process.env.NODE_ENV == 'production') ? 'source-map' : 'inline-source-map',
+	devtool: 'inline-source-map',
 	resolve: { 
-		modules: [path.resolve(__dirname, "src"), "node_modules"] 
+		modules: [path.resolve(__dirname, 'src'), 'node_modules'] 
 	},
 	devServer: {
-		contentBase: path.join(__dirname, "src")
+		contentBase: path.join(__dirname, 'src')
 	},
 	module: {
 		rules: [
 			{
 				test: /\.(js|jsx)$/, 
 				exclude: /node_modules/, 
-				use: ["babel-loader"] 
+				use: ['babel-loader'] 
 			},
 			{
 				test: /\.(css|scss)$/,
-				use: ["style-loader", "css-loader"],
+				use: ['style-loader', 'css-loader'],
 			},
 			{ 
 				test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
-				use: ["file-loader"] 
+				use: ['file-loader'] 
 			},
 		 ],
 	},
 	plugins: [
 		 new HtmlWebpackPlugin({
-			 template: path.join(__dirname, "src", "index.html"),
+			 template: path.join(__dirname, 'src', 'index.html'),
 		 }),
+		 new RemovePlugin({
+			 before: {
+				root: ".",
+				test: [
+					{
+						folder: './public',
+						method: () => true,
+						recursive: true
+					}
+				]
+			 }
+		 })
 	]
 };
