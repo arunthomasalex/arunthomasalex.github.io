dff6f32cffd8f1e5f270e6ec666bd661ce0086fc
arunthomasalex <arunthomasalex@gmail.com>
2021-05-09 18:32:14 +0530

    Changed the base path public to portfolio.

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const RemovePlugin = require('remove-files-webpack-plugin');
 module.exports = {
 	entry: path.join(__dirname, 'src', 'index.js'),
 	output: { 
-		path: path.join(__dirname, 'public'), 
+		path: path.join(__dirname, 'portfolio'), 
 		filename: 'build.[hash].js' 
 	},
 	mode: process.env.NODE_ENV || 'development',
