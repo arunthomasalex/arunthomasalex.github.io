73c32299533e0c36c5a13a2e34ba494d5062e06c
arunthomasalex <arunthomasalex@gmail.com>
2021-05-26 15:06:22 +0530

    Added about section

--- src/about/About.jsx
+++ src/about/About.jsx
@@ -1,7 +1,41 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import arun from '../home/ArunThomasAlex.jpg';
+import './socialmedia.png';
+import './about.scss';
 
 export default class About extends Component {
     render() {
-        return(<div>About</div>);
+        return(
+            <section className="about-section section">
+                <div className="container">
+                    <div className="row">
+                        <div className="section-title">
+                            <h2 data-heading="main info">
+                                About Me
+                            </h2>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="about-img">
+                            <div className="img-box inner-shadow">
+                                <img src={arun} alt="profile-pic" className="outer-shadow" />
+                            </div>
+                            <div className="social-links">
+                                <a href="#" className="social-link outer-shadow hover-in-shadow"><i className="fab fa-facebook-f"></i></a>
+                                <a href="#" className="social-link outer-shadow hover-in-shadow"><i className="fab fa-twitter"></i></a>
+                                <a href="#" className="social-link outer-shadow hover-in-shadow"><i className="fab fa-linkedin-in"></i></a>
+                            </div>
+                        </div>
+                        <div className="about-info">
+                            <p><span>Hi! My name is Arun Thomas Alex. I am a full stack developer.</span>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse</p>
+                            <p> cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                            <a href="#" className="btn-1 outer-shadow hover-in-shadow">Download CV</a>
+                            <a href="#" className="btn-1 outer-shadow hover-in-shadow">Hire Me</a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        );
     }
 }
--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 import arun from './ArunThomasAlex.jpg';
