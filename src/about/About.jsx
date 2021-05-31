c7bcde9c18ab365f2fd7546d21a96fabf3a2f648
arunthomasalex <arunthomasalex@gmail.com>
2021-05-31 12:04:39 +0530

    About section completed.

--- src/about/About.jsx
+++ src/about/About.jsx
@@ -6,7 +6,7 @@ import './about.scss';
 function SocialSiteLinks(props) {
     return (
         <div className="social-links">
-            {props.links.map(link => {
+            {props.links?.map(link => {
                 return <a href={link.url} target="_blank" key={link.name} className="social-outer-shadow social-hover-in-shadow"><i className={"fab " + link.name}></i></a>
             })}
         </div>
@@ -14,44 +14,21 @@ function SocialSiteLinks(props) {
 }
 
 function Skills(props) {
+    let classes = props.active ? "skills tab-content active" : "skills tab-content"
     return (
-        <div className="skills-tab">
-            <div className="skills tab-content active">
-                <div className="row">
-                    {props.skills.map(skill => {
-                        let percentCalc = "calc(" + skill.percentage + "% - 14px)";
-                        return (
-                            <div key={skill.name} className="skill-item">
-                                <p>{skill.name}</p>
-                                <div className="progress inner-shadow">
-                                    <div className="progress-bar" style={{ "width": percentCalc }}>
-                                        <span>{skill.percentage}%</span>
-                                    </div>
-                                </div>
-                            </div>
-                        );
-                    })}
-                </div>
-            </div>
-        </div>
-    )
-}
-
-function Experience(props) {
-    return (
-        <div className="experience tab-content">
-            <div className="row">
-                <div className="timeline">
+        <div className="row">
+            <div className={classes}>
+                <div className="skills-tab">
                     <div className="row">
-                        {props.experiences.map(experience => {
+                        {props.skills?.map(skill => {
+                            let percentCalc = "calc(" + skill.percentage + "% - 14px)";
                             return (
-                                <div key={experience.company} className="timeline-item">
-                                    <div className="timeline-item-inner outer-shadow">
-                                        <i className="fas briefcase icon"></i>
-                                        <span>{experience.duration}</span>
-                                        <h3>{experience.post}</h3>
-                                        <h4>{experience.company}</h4>
-                                        <p>{experience.description}</p>
+                                <div key={skill.name} className="skill-item">
+                                    <p>{skill.name}</p>
+                                    <div className="progress inner-shadow">
+                                        <div className="progress-bar" style={{ "width": percentCalc }}>
+                                            <span>{skill.percentage}%</span>
+                                        </div>
                                     </div>
                                 </div>
                             );
@@ -60,28 +37,59 @@ function Experience(props) {
                 </div>
             </div>
         </div>
+    )
+}
+
+function Experience(props) {
+    let classes = props.active ? "experience tab-content active" : "experience tab-content"
+    return (
+        <div className="row">
+            <div className={classes}>
+                <div className="row">
+                    <div className="timeline">
+                        <div className="row">
+                            {props.experiences?.map(experience => {
+                                return (
+                                    <div key={experience.company} className="timeline-item">
+                                        <div className="timeline-item-inner outer-shadow">
+                                            <i className="fas briefcase icon"></i>
+                                            <span>{experience.duration}</span>
+                                            <h3>{experience.post}</h3>
+                                            <h4>{experience.company}</h4>
+                                            <p>{experience.description}</p>
+                                        </div>
+                                    </div>
+                                );
+                            })}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
 function Education(props) {
-    console.log(props);
+    let classes = props.active ? "education tab-content active" : "education tab-content"
     return (
-        <div className="education tab-content">
-            <div className="row">
-                <div className="timeline">
-                    <div className="row">
-                        {props.educations.map(education => {
-                            return (
-                                <div key={education.course} className="timeline-item">
-                                    <div className="timeline-item-inner outer-shadow">
-                                        <i className="fas certificate icon"></i>
-                                        <span>{education.duration}</span>
-                                        <h3>{education.course}</h3>
-                                        <h4>{education.institution}</h4>
+        <div className="row">
+            <div className={classes}>
+                <div className="row">
+                    <div className="timeline">
+                        <div className="row">
+                            {props.educations.map(education => {
+                                return (
+                                    <div key={education.course} className="timeline-item">
+                                        <div className="timeline-item-inner outer-shadow">
+                                            <i className="fas certificate icon"></i>
+                                            <span>{education.duration}</span>
+                                            <h3>{education.course}</h3>
+                                            <h4>{education.institution}</h4>
+                                        </div>
                                     </div>
-                                </div>
-                            );
-                        })}
+                                );
+                            })}
+                        </div>
                     </div>
                 </div>
             </div>
@@ -91,8 +99,27 @@ function Education(props) {
 
 
 export default class About extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            skills: true,
+            experience: false,
+            education: false
+        }
+    }
+    changeTab(e) {
+        console.log(e.target.getAttribute("data-target"));
+        this.setState((state) => {
+            state = Object.assign(...Object.keys(state).map(k => ({ [k]: false })));
+            state[e.target.getAttribute("data-target")] = true;
+            console.log(state);
+            return state;
+        })
+    }
     render() {
         let { portfolio } = this.props;
+        let { skills, experience, education } = this.state;
+        let [skillStyle, experienceStyle, educationStyle] = [skills, experience, education].map(value => value ? "tab-item outer-shadow active" : "tab-item");
         return (
             <section className="about-section section" id="about">
                 <div className="container">
@@ -118,18 +145,14 @@ export default class About extends Component {
                     </div>
                     <div className="row">
                         <div className="about-tabs">
-                            <span className="tab-item outer-shadow active" data-taget=".skills">skills</span>
-                            <span className="tab-item" data-taget=".experience">experience</span>
-                            <span className="tab-item" data-taget=".education">education</span>
+                            <span className={skillStyle} data-target="skills" onClick={this.changeTab.bind(this)}>skills</span>
+                            <span className={experienceStyle} data-target="experience" onClick={this.changeTab.bind(this)}>experience</span>
+                            <span className={educationStyle} data-target="education" onClick={this.changeTab.bind(this)}>education</span>
                         </div>
-                        {portfolio && portfolio["skills"] && <Skills skills={portfolio["skills"]} />}
                     </div>
-                    <div className="row">
-                        {portfolio && portfolio["experiences"] && <Experience experiences={portfolio["experiences"]} />}
-                    </div>
-                    <div className="row">
-                        {portfolio && portfolio["educations"] && <Education educations={portfolio["educations"]}/>}
-                    </div>
+                    {skills && portfolio && <Skills skills={portfolio["skills"]} active={skills} />}
+                    {experience && portfolio && <Experience experiences={portfolio["experiences"]} active={experience}/>}
+                    {education && portfolio && <Education educations={portfolio["educations"]} active={education} />}
                 </div>
             </section>
         );
